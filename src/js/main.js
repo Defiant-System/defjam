bb97ad3f85dfa840000ea3189099a48b6bc253ee
Hakan Bilgin <hbi99@hotmail.com>
2020-05-30 18:29:28 +0200

    more ui + panel toggles

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 
+// temp
 let file = `<data>
 		<Tracks>
 			<i name="Drums" />
@@ -12,10 +13,13 @@ let file = `<data>
 		</IoMaster>
 	</data>`;
 
+
 const jam = {
 	init() {
 		// fast references
 		this.content = window.find("content");
+		this.panelLeft = window.find(".panel-left");
+		this.panelBottom = window.find(".panel-bottom");
 
 		// bind event handlers
 		this.content.on("mousedown", ".knob, .pan-knob", this.doKnob);
@@ -25,6 +29,7 @@ const jam = {
 	},
 	dispatch(event) {
 		let self = jam,
+			isOn,
 			el;
 		//console.log(event);
 		switch (event.type) {
@@ -36,6 +41,18 @@ const jam = {
 					template: "session",
 					prepend: self.content.find(".session-wrapper")
 				});
+				break;
+			case "toggle-work-panel":
+				el = event.el;
+				isOn = el.hasClass("toggled");
+				el.toggleClass("toggled", isOn);
+				self.panelLeft.toggleClass("hide", isOn);
+				break;
+			case "toggle-rack-panel":
+				el = event.el;
+				isOn = el.hasClass("toggled");
+				el.toggleClass("toggled", isOn);
+				self.panelBottom.toggleClass("hide", isOn);
 				break;
 		}
 	},

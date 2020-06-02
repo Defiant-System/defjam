eef38cfe32033b75a086a97640d874ad627aeef1
Hakan Bilgin <hbi99@hotmail.com>
2020-06-02 21:22:29 +0200

    testing app cache

--- src/js/modules/audio.js
+++ src/js/modules/audio.js
@@ -6,7 +6,7 @@ const Audio = {
 		let buffer = await audioContext.decodeAudioData(arrayBuffer);
 		let data = this.visualize(buffer);
 
-		this.draw(ctx, data);
+		this.draw({ url, ctx, data });
 	},
 	visualize(buffer) {
 		let rawData = buffer.getChannelData(0);
@@ -28,31 +28,36 @@ const Audio = {
 
 		return filteredData;
 	},
-	draw(ctx, data) {
-		ctx.clearRect(0, 0, 1e4, 1e4);
-		ctx.save();
-		ctx.lineWidth = 1;
-		ctx.strokeStyle = "#71a1ca";
-		ctx.translate(0.5, 0.5);
+	draw(opt) {
+		opt.ctx.clearRect(0, 0, 1e4, 1e4);
+		opt.ctx.save();
+		opt.ctx.lineWidth = 1;
+		opt.ctx.strokeStyle = "#71a1ca";
+		opt.ctx.translate(0.5, 0.5);
 
-		data.map((v, x) => {
+		opt.data.map((v, x) => {
 			//let y = (v * 11);
-			// ctx.beginPath();
-			// ctx.moveTo(5 + (x * 2), 15 - y);
-			// ctx.lineTo(5 + (x * 2), 1 + y + 15);
-			// ctx.stroke();
+			// opt.ctx.beginPath();
+			// opt.ctx.moveTo(5 + (x * 2), 15 - y);
+			// opt.ctx.lineTo(5 + (x * 2), 1 + y + 15);
+			// opt.ctx.stroke();
 
 			let y = (v * 11),
 				x1 = 2 + (x * 3),
 				y1 = 16 - y,
 				x2 = 1,
 				y2 = (y * 2);
-			ctx.strokeRect(x1, y1, x2, y2);
+			opt.ctx.strokeRect(x1, y1, x2, y2);
 		});
 
-		ctx.restore();
+		opt.ctx.restore();
 
-		defiant.cache({ url: "/ant/app/", data: "123" });
+		// let url = "test123.png",
+		// 	uri = opt.ctx.canvas.toDataURL();
+		// defiant.cache({ url, uri });
+
+		let url = "test123.png";
+		opt.ctx.canvas.toBlob(blob => defiant.cache({ url, blob }));
 	}
 };
 

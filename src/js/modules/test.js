6f3416f210879359e67422bdb0dfa3b3cb1ba182
Hakan Bilgin <hbi99@hotmail.com>
2023-01-21 00:40:55 +0100

    more UI

--- src/js/modules/test.js
+++ src/js/modules/test.js
@@ -1,6 +1,6 @@
 
 let Test = {
 	init() {
-		window.find(`.ball-button[data-click="show-arrangement-view"]`).trigger("click");
+		// window.find(`.ball-button[data-click="show-arrangement-view"]`).trigger("click");
 	}
 };

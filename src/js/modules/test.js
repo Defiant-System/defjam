ab3cff93a12dbf4b7c72b3da7fa99c3a919fe419
Hakan Bilgin <hbi99@hotmail.com>
2023-02-18 22:51:43 +0100

    amplitude envelope UI

--- src/js/modules/test.js
+++ src/js/modules/test.js
@@ -1,8 +1,9 @@
 
 let Test = {
 	init() {
-		window.find(`.ball-button[data-click="show-arrangement-view"]`).trigger("click");
-		window.find(`.foot-midi`).trigger("click");
+		// window.find(`.ball-button[data-click="show-arrangement-view"]`).trigger("click");
+		window.find(`.foot-devices`).trigger("click");
+		// window.find(`.foot-midi`).trigger("click");
 		// window.find(`.foot-drumkit`).trigger("click");
 	}
 };

a2b8c854b7157181bd5323878f7be30d6194985b
Hakan Bilgin <hbi99@hotmail.com>
2023-02-23 16:53:14 +0100

    midi editor lasso

--- src/js/sections/midiEditor.js
+++ src/js/sections/midiEditor.js
@@ -7,14 +7,67 @@
 		this.els = {
 			doc: $(document),
 			el: window.find(".midi-note-editor"),
+			lasso: window.find(".midi-note-editor .lasso"),
 		};
 
 		// bind event handler
+		this.els.el.find(".row-body .col-right .body-frame").on("mousedown", this.doLasso);
 		this.els.el.find(".row-body .body-frame ul").on("mousedown", this.doNoteRuler);
 		this.els.el.find(".col-right .body-frame ul").on("mousedown", this.doNoteBars);
 	},
 	dispatch(event) {
 
+	},
+	doLasso(event) {
+		let APP = defjam,
+			Self = APP.midiEditor,
+			Drag = Self.drag,
+			el;
+		switch (event.type) {
+			case "mousedown":
+				// prevent default behaviour
+				event.preventDefault();
+
+				// prepare drag object
+				let el = Self.els.lasso.removeClass("hidden"),
+					pEl = Self.els.el,
+					oY = parseInt(el.cssProp("--oY"), 10),
+					oX = parseInt(el.cssProp("--oX"), 10),
+					cY = event.offsetY,
+					cX = event.offsetX,
+					eX = event.clientX,
+					eY = event.clientY,
+					limit = {
+						minY: 0,
+						maxY: 500,
+					},
+					min_ = Math.min,
+					max_ = Math.max;
+				Self.drag = { el, oX, oY, cX, cY, eX, eY, limit, min_, max_ };
+				
+
+				// prevent mouse from triggering mouseover
+				APP.els.content.addClass("hide-cursor");
+				// bind event handlers
+				Self.els.doc.on("mousemove mouseup", Self.doLasso);
+				break;
+			case "mousemove":
+				let top = Drag.cY,
+					left = Drag.cX,
+					height = event.clientY - Drag.eY,
+					width = event.clientX - Drag.eX;
+
+				// UI update
+				Drag.el.css({ top, left, width, height });
+				break;
+			case "mouseup":
+				Drag.el.addClass("hidden").css({ width: 0, height: 0 });
+				// remove class
+				APP.els.content.removeClass("hide-cursor");
+				// unbind event handlers
+				Self.els.doc.off("mousemove mouseup", Self.doLasso);
+				break;
+		}
 	},
 	doNoteRuler(event) {
 		let APP = defjam,
@@ -33,11 +86,13 @@
 					offsetY = parseInt(el.cssProp("--oY"), 10),
 					clickX = event.clientX,
 					clickY = event.clientY,
-					min = { y: 0 },
-					max = { y: viewH - allH },
+					limit = {
+						minY: 0,
+						maxY: viewH - allH,
+					},
 					min_ = Math.min,
 					max_ = Math.max;
-				Self.drag = { el, clickX, clickY, offsetY, min, max, min_, max_ };
+				Self.drag = { el, clickX, clickY, offsetY, limit, min_, max_ };
 				
 				// prevent mouse from triggering mouseover
 				APP.els.content.addClass("hide-cursor");
@@ -45,7 +100,7 @@
 				Self.els.doc.on("mousemove mouseup", Self.doNoteRuler);
 				break;
 			case "mousemove":
-				let oY = Drag.max_(Drag.min_(Drag.offsetY - Drag.clickY + event.clientY, Drag.min.y), Drag.max.y);
+				let oY = Drag.max_(Drag.min_(Drag.offsetY - Drag.clickY + event.clientY, Drag.limit.minY), Drag.limit.maxY);
 				Drag.el.css({ "--oY": `${oY}px` });
 				break;
 			case "mouseup":
@@ -73,11 +128,13 @@
 					offsetX = parseInt(el.cssProp("--oX"), 10),
 					clickX = event.clientX,
 					clickY = event.clientY,
-					min = { x: 0 },
-					max = { x: Math.max(viewH - allH, 0) },
+					limit = {
+						minX: 0,
+						maxX: Math.max(viewW - allW, 0),
+					},
 					min_ = Math.min,
 					max_ = Math.max;
-				Self.drag = { el, clickX, clickY, offsetX, min, max, min_, max_ };
+				Self.drag = { el, clickX, clickY, offsetX, limit, min_, max_ };
 				
 				// prevent mouse from triggering mouseover
 				APP.els.content.addClass("hide-cursor");
@@ -85,7 +142,7 @@
 				Self.els.doc.on("mousemove mouseup", Self.doNoteBars);
 				break;
 			case "mousemove":
-				let oX = Drag.max_(Drag.min_(Drag.offsetX - Drag.clickX + event.clientX, Drag.min.x), Drag.max.x);
+				let oX = Drag.max_(Drag.min_(Drag.offsetX - Drag.clickX + event.clientX, Drag.limit.minX), Drag.limit.maxX);
 				Drag.el.css({ "--oX": `${oX}px` });
 				break;
 			case "mouseup":

a04fbd5ea5b10b27fe86cf2dcae9e606ecf34023
Hakan Bilgin <hbi99@hotmail.com>
2023-02-23 14:25:36 +0100

    midi note editor UX

--- src/js/sections/midiEditor.js
+++ src/js/sections/midiEditor.js
@@ -23,10 +23,35 @@
 			el;
 		switch (event.type) {
 			case "mousedown":
+				// prevent default behaviour
+				event.preventDefault();
+				// prepare drag object
+				let el = Self.els.el,
+					allH = el.find(".row-body .col-right .body-frame").prop("offsetHeight"),
+					viewH = el.find(".row-body .col-right .box-body").prop("offsetHeight"),
+					offsetY = parseInt(el.cssProp("--oY"), 10),
+					clickX = event.clientX,
+					clickY = event.clientY,
+					min = { y: 0 },
+					max = { y: viewH - allH },
+					min_ = Math.min,
+					max_ = Math.max;
+				Self.drag = { el, clickX, clickY, offsetY, min, max, min_, max_ };
+				console.log( viewH, allH );
+				// prevent mouse from triggering mouseover
+				APP.els.content.addClass("hide-cursor");
+				// bind event handlers
+				Self.els.doc.on("mousemove mouseup", Self.doNoteRuler);
 				break;
 			case "mousemove":
+				let oY = Drag.max_(Drag.min_(Drag.offsetY - Drag.clickY + event.clientY, Drag.min.y), Drag.max.y);
+				Drag.el.css({ "--oY": `${oY}px` });
 				break;
 			case "mouseup":
+				// remove class
+				APP.els.content.removeClass("hide-cursor");
+				// unbind event handlers
+				Self.els.doc.off("mousemove mouseup", Self.doNoteRuler);
 				break;
 		}
 	},

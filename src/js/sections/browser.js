7fab2b9f75dc6d780103611e1fd2f851f12af180
Hakan Bilgin <hbi99@hotmail.com>
2023-02-25 20:14:56 +0100

    minor browser UI / UX

--- src/js/sections/browser.js
+++ src/js/sections/browser.js
@@ -5,17 +5,13 @@
 	init() {
 		// fast references
 		this.els = {
+			soundsBody: window.find(".sounds-body"),
 			audioChart: window.find(".audio-chart"),
 		};
 
-		Audio.visualizeFile({
-				url: "~/sounds/909 Core Kit/Kick.ogg",
-				width: 202,
-				height: 33,
-				color: "#71a1ca",
-			})
-			 .then(path => this.els.audioChart.css({ "background-image": `url(${path})` }));
-
+		// temp
+		this.els.soundsBody.find(`.leaf[data-path="Kick.ogg"]`).trigger("click");
+		// this.dispatch({ type: "preview-audio", path: "Kick.ogg" });
 	},
 	dispatch(event) {
 		let APP = defjam,
@@ -25,7 +21,36 @@
 			el;
 		//console.log(event);
 		switch (event.type) {
-			case "show-sounds":
+			case "do-tree-leaf":
+				el = $(event.target);
+				el.parents(".box-body").find(".leaf.active").removeClass("active");
+
+				if (el.hasClass("box-body")) {
+					// hide audio preview
+					return Self.els.soundsBody.removeClass("show-audio-preview");
+				} else if (el.hasClass("icon-folder")) {
+					el.parent().toggleClass("expanded", el.parent().hasClass("expanded"));
+					return;
+				} else if (el.parent().hasClass("leaf")) {
+					el = el.parent();
+				}
+				el.addClass("active");
+
+				if (el.data("path")) {
+					return Self.dispatch({ type: "preview-audio", path: el.data("path") });
+				}
+				break;
+			case "preview-audio":
+				Audio.visualizeFile({
+					url: `~/sounds/909 Core Kit/${event.path}`,
+					width: 202,
+					height: 33,
+					color: "#71a1ca",
+				})
+				.then(path => {
+					Self.els.soundsBody.addClass("show-audio-preview");
+					Self.els.audioChart.css({ "background-image": `url(${path})` });
+				});
 				break;
 		}
 	}

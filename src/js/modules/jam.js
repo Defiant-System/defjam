760491fa603b97dc50a1308327864277ae93fd66
Hakan Bilgin <hbi99@hotmail.com>
2023-03-09 16:54:02 +0100

    Update jam.js

--- src/js/modules/jam.js
+++ src/js/modules/jam.js
@@ -5,6 +5,40 @@ const Jam = {
 		this.display.init();
 		// defaults
 		this.stop();
+
+		// temp
+		this.meter1 = new Tone.Meter({ channels: 1 });
+		this.channel1 = new Tone.Channel().connect(this.meter1).toDestination();
+		// channel 1
+		let data = {
+				urls: {
+					0: "3609.ogg", // kick
+					1: "3612.ogg", // snare
+					2: "3639.ogg", // hihat
+					3: "3507.ogg", // clave
+				},
+				fadeOut: "4n",
+				baseUrl: "/cdn/audio/samples/",
+			},
+			drumkit = new Tone.Players(data).connect(this.channel1),
+			beat = 0,
+			grid = [
+				[1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0], // kick
+				[0,0,1,0,0,0,1,0,0,0,1,0,0,0,1,0], // snare
+				[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1], // hihat
+				[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], // clave
+			],
+			clip1 = time => {
+				grid.map((row, index) => {
+					if (row[beat]) {
+						drumkit.player(index).start(time, 0, "4n");
+					}
+				});
+				beat = (beat + 1) % 16;
+			};
+
+		Tone.Transport.scheduleRepeat(clip1, "8n");
+
 	},
 	start() {
 		this._stopped = false;
@@ -12,12 +46,18 @@ const Jam = {
 		this.update();
 	},
 	stop() {
+		// temp (rewind)
+		Tone.Transport.position = "0:0:0";
+
 		this._stopped = true;
 		Tone.Transport.stop();
 	},
 	update() {
 		if (this._stopped) return;
 		// do calculations
+		let bars = Tone.Transport.position.split(".")[0].split(":");
+		if (bars[0].length < 2) bars[0] = " "+ bars[0];
+		this.display.render({ bars: bars.join(" ") });
 
 		// render whats need to be rendered
 		this.render();
@@ -25,7 +65,7 @@ const Jam = {
 		requestAnimationFrame(this.update.bind(this));
 	},
 	render() {
-		console.log( Tone.Transport.position );
+		
 	},
 	display: {
 		init() {
@@ -52,7 +92,7 @@ const Jam = {
 		},
 		render(data={}) {
 			let time = data.time || `00 00 00`,
-				bars = data.bars || ` 1 1 1`,
+				bars = data.bars || ` 0 0 0`,
 				tempo = data.tempo || `120`;
 			// clear canvas
 			this.ctx.clearRect(0, 0, this.width, this.height);

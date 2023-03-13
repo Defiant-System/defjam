13e9ec7946fd1c679bad074f420adf680e0785c4
Hakan Bilgin <hbi99@hotmail.com>
2023-03-13 15:00:55 +0100

    rendering drumkit clip correctly

--- src/js/classes/file.js
+++ src/js/classes/file.js
@@ -21,10 +21,25 @@ class File {
 			case "load-project":
 				// prepare track clip notes
 				Self._file.data.selectNodes(`//Track/Clip/b[@n]`).map(xNote => {
-					let key = xNote.getAttribute("n").slice(0,1),
-						octave = +xNote.getAttribute("n").slice(1),
+					let note = xNote.getAttribute("n"),
+						key = note.slice(0,-1),
+						octave = +note.slice(-1),
 						y = ((7 - octave) * 12) + (11 - OCTAVE.indexOf(key)),
-						w = +xNote.getAttribute("d");
+						w = +xNote.getAttribute("d"),
+						xTrack = xNote.parentNode.parentNode;
+					if (xTrack.getAttribute("type") == "drumkit") {
+						let padMap = xTrack.selectNodes(`.//Pads/Pad[@sample]`).map(xPad =>
+							({
+								kI: +xPad.getAttribute("kI"),
+								key: xPad.getAttribute("key"),
+								sample: xPad.getAttribute("sample"),
+								name: xPad.getAttribute("name"),
+							}))
+							// sort pad array using key index (as viewed in UI)
+							.sort((a, b) => a.kI - b.kI);
+						// change y value
+						y = padMap.findIndex(p => p.key === note);
+					}
 					xNote.setAttribute("y", y);
 					xNote.setAttribute("w", w);
 				});
@@ -32,8 +47,6 @@ class File {
 				APP.session.dispatch({ type: "render-file", file: Self._file });
 				// load file instruments
 				Jam.loadProject(Self._file);
-				// TODO: auto select a track
-
 				break;
 		}
 	}
